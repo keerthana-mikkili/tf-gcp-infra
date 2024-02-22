022e7a77166f9302bac629745f168637e52d65d3
Keerthana Mikkili <mikkili.k@northeastern.edu>
2024-02-22 12:37:09 -0500

    Added VM Configuration

--- main.tf
+++ main.tf
@@ -10,8 +10,9 @@ terraform {
 provider "google" {
   project = var.project_id
   region  = var.region
-  zone    = var.region
+  zone    = var.zone
 }
+
 resource "google_compute_network" "vpc_network" {
   count                           = length(var.vpcs)
   name                            = var.vpcs[count.index].vpc_name
@@ -28,6 +29,7 @@ resource "google_compute_subnetwork" "webapp" {
   network                  = google_compute_network.vpc_network[count.index].self_link
   private_ip_google_access = var.vpcs[count.index].privateipgoogleaccess
 }
+
 resource "google_compute_subnetwork" "db" {
   count                    = length(var.vpcs)
   name                     = var.vpcs[count.index].dbsubnet_name
@@ -45,4 +47,58 @@ resource "google_compute_route" "webapp_route" {
   priority         = 1000
   next_hop_gateway = var.nexthopgateway
 }
+
+resource "google_compute_firewall" "allowtraffic_applicationport" {
+  count   = length(var.vpcs)
+  name    = "allow-traffic-application-port-${count.index}"
+  network = google_compute_network.vpc_network[count.index].name
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.app_port]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "denytraffic_sshport" {
+  count   = length(var.vpcs)
+  name    = "deny-traffic-ssh-port-${count.index}" 
+  network = google_compute_network.vpc_network[count.index].name
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+resource "google_compute_instance" "vm_instance" {
+  count = length(var.vpcs)
+
+  name         = var.vm_name
+  zone         = var.zone
+  machine_type = var.vm_machine_type
+
+  boot_disk {
+    initialize_params {
+      image = var.vm_image
+      type  = var.vm_disk_type
+      size  = var.vm_disk_size_gb
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network[count.index].name
+    subnetwork = google_compute_subnetwork.webapp[count.index].name
+
+    access_config {
+      //Assigns External IP to the VM Instance
+    }
+  }
+}
+
+
 
